79497b68f7b4b27341e027eb70912306460326fd
Charly Aguirre <charagmz@gmail.com>
2022-07-17 14:41:40 -0500

    rutas privadas y publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
+import { PrivateRoute } from "../heroes/routes/PrivateRoute";
+import { PublicRoute } from "../heroes/routes/PublicRoute";
 
 export const AppRouter = () => {
     return (
@@ -8,8 +10,29 @@ export const AppRouter = () => {
             
             <Routes>
                 
-                <Route path="login" element={<LoginPage />} />
-                <Route path="/*" element={<HeroesRoutes />} />
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+
+                {/* Otra forma de hacer publico el login*/}
+                {/* <Route path="login/*" element={
+                    <PublicRoute>
+                        <Routes>
+                            <Route path="/*" element={<LoginPage />} />
+                        </Routes>
+                    </PublicRoute>
+                } /> */}
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                } />
+
+                {/* <Route path="login" element={<LoginPage />} /> */}
+                {/* <Route path="/*" element={<HeroesRoutes />} /> */}
             </Routes>
         </>
     )
